940548b2eefda4c44ee19504bf1cd7ea6c67174b
Artemiy Karelov <karelov.artemiy@mail.ru>
2022-09-26 21:02:15 +0300

    add counter block

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+    const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div>
+              <h2>Счетчик:</h2>
+              <h1>{count}</h1>
+              <button disabled={count ? false : true} onClick={() => setCount(count-1)} id='minus' className="minus">- Минус</button>
+              <button onClick={() => setCount(count+1)} className="plus">Плюс +</button>
+          </div>
+      </div>
   );
 }
 
