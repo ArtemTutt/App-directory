3331bead5ecfde855bb5fa23ae53c43b622047a6
Artemiy Karelov <karelov.artemiy@mail.ru>
2022-09-27 20:39:11 +0300

    add modal window

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.scss';
 
 function App() {
 
-    const [count, setCount] = useState(0);
 
-  return (
-      <div className="App">
-          <div>
-              <h2>Счетчик:</h2>
-              <h1>{count}</h1>
-              <button disabled={count ? false : true} onClick={() => setCount(count-1)} id='minus' className="minus">- Минус</button>
-              <button onClick={() => setCount(count+1)} className="plus">Плюс +</button>
-          </div>
-      </div>
-  );
+    return (
+        <div className="App">
+            <button className="open-modal-btn">✨ Открыть окно</button>
+            {/*<div className="overlay">*/}
+            {/*    <div className="modal">*/}
+            {/*        <svg height="200" viewBox="0 0 200 200" width="200">*/}
+            {/*            <title />*/}
+            {/*            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />*/}
+            {/*        </svg>*/}
+            {/*        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+        </div>
+    );
 }
 
 export default App;
