295ad78b91d1c078e6319ab50a2a8998b5a170e4
Artemiy Karelov <karelov.artemiy@mail.ru>
2022-09-28 19:28:50 +0300

    add list-users

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import React, { useState } from 'react';
-import Game from "./Game";
-import './App.scss';
-import {questions} from "./Data/q";
-import Result from "./Result";
+import './index.scss';
+import {Success} from "./components/Success";
+import {Users} from "./components/Users";
 
 
-
-
-// function Result({correct}) {
-//
-// }
-
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-
-    const [step, setStep] = useState(0); // для получения вопроса, index нашего вопроса
-    const [correct, setCorrect] = useState(0); // для получени правильного ответа
-    const  question = questions[step];
-
-    const onCLickVariant = (index) => {
-        console.log(step, index)
-        setStep(step + 1)
-
-        if(index === question.correct) {
-            setCorrect(correct + 1);
-        }
-    }
-
-
     return (
         <div className="App">
-            {step !== questions.length ? <Game step={step} question={question} onCLickVariant={onCLickVariant} /> :
-                (<Result correct={correct}>
-                    <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-                </Result>)
-            }
-            {/*{step === 2 && <Result/>}*/}
+            <Users />
+            {/* <Success /> */}
         </div>
     );
 }
