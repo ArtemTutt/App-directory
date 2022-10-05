6636d49090bffd6cb196a10ee4c7bd744abd15d1
Artemiy Karelov <karelov.artemiy@mail.ru>
2022-10-05 19:11:09 +0300

    Создал скелет фото альбома

--- src/App.js
+++ src/App.js
@@ -1,63 +1,54 @@
 import React, {useEffect, useRef, useState} from 'react';
 import './index.scss';
-import {Block} from "./Block";
-import axios from "axios";
+
 
 //https://cdn.cur.su/api/latest.json
 
 
+function Collection({ name, images }) {
+    return (
+        <div className="collection">
+            <img className="collection__big" src={images[0]} alt="Item" />
+            <div className="collection__bottom">
+                <img className="collection__mini" src={images[1]} alt="Item" />
+                <img className="collection__mini" src={images[2]} alt="Item" />
+                <img className="collection__mini" src={images[3]} alt="Item" />
+            </div>
+            <h4>{name}</h4>
+        </div>
+    );
+}
+
 function App() {
-
-    // const [rates, setRates] = useState({});
-    const ratesRef = useRef({})
-
-    const [formCurrency, setFromCurrency] = useState('RUB')
-    const [toCurrency, setToCurrency] = useState('USD');
-
-    const [fromPrice, setFromPrice] = useState(0)
-    const [toPrice, setToPrice] = useState(1)
-
-    useEffect(() => {
-        axios.get('https://cdn.cur.su/api/latest.json')
-            .then(data => {
-                // setRates(data.data.rates);
-                ratesRef.current = data.data.rates
-                onChangeToPrice(1)
-            }).catch(err => {
-                console.error(err)
-                alert('Произошла ошибка с сервером')
-            })
-    }, [])
-
-    const onChangeFromPrice = (value) => {
-        const price = value / ratesRef.current[formCurrency];
-        const result = price * ratesRef.current[toCurrency];
-        setToPrice(result.toFixed(3));
-        setFromPrice(value);
-    }
-
-    const onChangeToPrice = (value) => {
-        const result = (ratesRef.current[formCurrency] / ratesRef.current[toCurrency]) * value;
-        setFromPrice(result.toFixed(3));
-        setToPrice(value);
-    }
-
-    useEffect(() => {         // отслеживание за переключением валют
-        onChangeFromPrice(fromPrice);
-    }, [formCurrency]);
-
-    useEffect(() => {         // отслеживание за переключением валют
-        onChangeToPrice(toPrice);
-    }, [toCurrency]);
-
-
     return (
         <div className="App">
-            <Block value={fromPrice} currency={formCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice}/>
-            <div>
-                <span>{`<- ->`}</span>
+            <h1>Моя коллекция фотографий</h1>
+            <div className="top">
+                <ul className="tags">
+                    <li className="active">Все</li>
+                    <li>Горы</li>
+                    <li>Море</li>
+                    <li>Архитектура</li>
+                    <li>Города</li>
+                </ul>
+                <input className="search-input" placeholder="Поиск по названию" />
             </div>
-            <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice}/>
+            <div className="content">
+                <Collection
+                    name="Путешествие по миру"
+                    images={[
+                        'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+                        'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+                    ]}
+                />
+            </div>
+            <ul className="pagination">
+                <li>1</li>
+                <li className="active">2</li>
+                <li>3</li>
+            </ul>
         </div>
     );
 }
